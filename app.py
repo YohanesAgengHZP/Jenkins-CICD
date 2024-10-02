eaa6672e6eea3cd660ab70eeee733a1d895ae965
shiblystssolution <shiblystssolution@gmail.com>
2024-10-02 16:11:17 +0700

    adding jenkins

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print('Hello World')
-print ('using python')
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Jenkins CI/CD with Docker!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
